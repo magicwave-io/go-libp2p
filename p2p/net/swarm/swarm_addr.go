e8c283ae5d7ae3ddfc0896fa61b138fd949e907c
a.gorulev <a.gorulev@kernelics.com>
2024-12-19 15:12:38 +0000

    magicwave.io

---
+++ p2p/net/swarm/swarm_addr.go
@@ -0,0 +1,27 @@
+package swarm
+
+import (
+	conn "github.com/libp2p/go-libp2p/p2p/net/conn"
+	addrutil "github.com/libp2p/go-libp2p/p2p/net/swarm/addr"
+
+	ma "github.com/jbenet/go-multiaddr"
+)
+
+// ListenAddresses returns a list of addresses at which this swarm listens.
+func (s *Swarm) ListenAddresses() []ma.Multiaddr {
+	listeners := s.swarm.Listeners()
+	addrs := make([]ma.Multiaddr, 0, len(listeners))
+	for _, l := range listeners {
+		if l2, ok := l.NetListener().(conn.Listener); ok {
+			addrs = append(addrs, l2.Multiaddr())
+		}
+	}
+	return addrs
+}
+
+// InterfaceListenAddresses returns a list of addresses at which this swarm
+// listens. It expands "any interface" addresses (/ip4/0.0.0.0, /ip6/::) to
+// use the known local interfaces.
+func (s *Swarm) InterfaceListenAddresses() ([]ma.Multiaddr, error) {
+	return addrutil.ResolveUnspecifiedAddresses(s.ListenAddresses(), nil)
+}
