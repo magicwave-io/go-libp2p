9ff4d4490f7967b6b99050123c2d4d92e1dd635b
a.gorulev <a.gorulev@kernelics.com>
2024-12-19 20:12:50 +0000

    magicwave.io

---
+++ p2p/protocol/ping/ping_test.go
@@ -0,0 +1,78 @@
+package ping
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	peer "github.com/libp2p/go-libp2p-peer"
+	pstore "github.com/libp2p/go-libp2p-peerstore"
+	swarmt "github.com/libp2p/go-libp2p-swarm/testing"
+	bhost "github.com/libp2p/go-libp2p/p2p/host/basic"
+)
+
+func TestPing(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	h1 := bhost.New(swarmt.GenSwarm(t, ctx))
+	h2 := bhost.New(swarmt.GenSwarm(t, ctx))
+
+	err := h1.Connect(ctx, pstore.PeerInfo{
+		ID:    h2.ID(),
+		Addrs: h2.Addrs(),
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ps1 := NewPingService(h1)
+	ps2 := NewPingService(h2)
+
+	testPing(t, ps1, h2.ID())
+	testPing(t, ps2, h1.ID())
+}
+
+func TestPingLegacyFallback(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	h1 := bhost.New(swarmt.GenSwarm(t, ctx))
+	h2 := bhost.New(swarmt.GenSwarm(t, ctx))
+
+	err := h1.Connect(ctx, pstore.PeerInfo{
+		ID:    h2.ID(),
+		Addrs: h2.Addrs(),
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ps1 := NewPingService(h1)
+	NewPingService(h2)
+	h2.RemoveStreamHandler(ID)
+
+	testPing(t, ps1, h2.ID())
+}
+
+func testPing(t *testing.T, ps *PingService, p peer.ID) {
+	pctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ts, err := ps.Ping(pctx, p)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 0; i < 5; i++ {
+		select {
+		case took, ok := <-ts:
+			if !ok {
+				t.Fatal("ping failed")
+			}
+			t.Logf("ping took: %s", took)
+		case <-time.After(time.Second * 4):
+			t.Fatal("failed to receive ping")
+		}
+	}
+
+}
