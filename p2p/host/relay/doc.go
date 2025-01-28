11f1c72f7347a01ecc5c3c2ff26cbdc68e8a0b68
a.gorulev <a.gorulev@kernelics.com>
2025-01-28 10:13:14 +0000

    magicwave.io

---
+++ p2p/host/relay/doc.go
@@ -0,0 +1,31 @@
+/*
+The relay package contains host implementations that automatically
+advertise relay addresses when the presence of NAT is detected. This
+feature is dubbed `autorelay`.
+
+Warning: the internal interfaces are unstable.
+
+System Components:
+  - AutoNATService instances -- see https://github.com/libp2p/go-libp2p-autonat-svc
+  - One or more relays, instances of `RelayHost`
+  - The autorelayed hosts, instances of `AutoRelayHost`.
+
+How it works:
+
+- AutoNATService
+  `AutoNATService` instances are instantiated in the
+  bootstrappers (or other well known publicly reachable hosts)
+
+- RelayHost
+  RelayHosts provide Relay Hop services, and advertise through the DHT
+  in the `/libp2p/relay` namespace
+
+- AutoRelayHost
+  AutoRelayHosts passively discover autonat service instances and test dialability of
+  their listen address set through them.  When the presence of NAT is detected,
+  they discover relays through the DHT, connect to some of them and begin
+  advertising relay addresses.  The new set of addresses is propagated to
+  connected peers through the `identify/push` protocol.
+
+*/
+package relay
---
+++ p2p/host/relay/relay.go
@@ -0,0 +1,49 @@
+package relay
+
+import (
+	"context"
+	"time"
+
+	basic "github.com/libp2p/go-libp2p/p2p/host/basic"
+
+	discovery "github.com/libp2p/go-libp2p-discovery"
+	host "github.com/libp2p/go-libp2p-host"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+var (
+	AdvertiseBootDelay = 5 * time.Second
+)
+
+// RelayHost is a Host that provides Relay services.
+type RelayHost struct {
+	*basic.BasicHost
+	advertise discovery.Advertiser
+	addrsF    basic.AddrsFactory
+}
+
+// Warning:  This function should not be used directly by clients activating
+// relay. NewRelayHost is used internally by the libp2p.New generator. To enable
+// relay, use the EnableRelay Option. https://godoc.org/github.com/libp2p/go-libp2p#EnableRelay
+func NewRelayHost(ctx context.Context, bhost *basic.BasicHost, advertise discovery.Advertiser) *RelayHost {
+	h := &RelayHost{
+		BasicHost: bhost,
+		addrsF:    bhost.AddrsFactory,
+		advertise: advertise,
+	}
+	bhost.AddrsFactory = h.hostAddrs
+	go func() {
+		select {
+		case <-time.After(AdvertiseBootDelay):
+			discovery.Advertise(ctx, advertise, RelayRendezvous)
+		case <-ctx.Done():
+		}
+	}()
+	return h
+}
+
+func (h *RelayHost) hostAddrs(addrs []ma.Multiaddr) []ma.Multiaddr {
+	return filterUnspecificRelay(h.addrsF(addrs))
+}
+
+var _ host.Host = (*RelayHost)(nil)
