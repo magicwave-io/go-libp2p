3dcc55512525b2467879d60d768946a16a80ed8d
a.gorulev <a.gorulev@kernelics.com>
2025-01-28 10:14:42 +0000

    magicwave.io

---
+++ p2p/test/util/util.go
@@ -0,0 +1,41 @@
+package testutil
+
+import (
+	"testing"
+
+	pstore "github.com/ipfs/go-libp2p-peerstore"
+	bhost "github.com/ipfs/go-libp2p/p2p/host/basic"
+	metrics "github.com/ipfs/go-libp2p/p2p/metrics"
+	inet "github.com/ipfs/go-libp2p/p2p/net"
+	swarm "github.com/ipfs/go-libp2p/p2p/net/swarm"
+	tu "github.com/ipfs/go-libp2p/testutil"
+
+	ma "github.com/jbenet/go-multiaddr"
+	context "golang.org/x/net/context"
+)
+
+func GenSwarmNetwork(t *testing.T, ctx context.Context) *swarm.Network {
+	p := tu.RandPeerNetParamsOrFatal(t)
+	ps := pstore.NewPeerstore()
+	ps.AddPubKey(p.ID, p.PubKey)
+	ps.AddPrivKey(p.ID, p.PrivKey)
+	n, err := swarm.NewNetwork(ctx, []ma.Multiaddr{p.Addr}, p.ID, ps, metrics.NewBandwidthCounter())
+	if err != nil {
+		t.Fatal(err)
+	}
+	ps.AddAddrs(p.ID, n.ListenAddresses(), pstore.PermanentAddrTTL)
+	return n
+}
+
+func DivulgeAddresses(a, b inet.Network) {
+	id := a.LocalPeer()
+	addrs := a.Peerstore().Addrs(id)
+	b.Peerstore().AddAddrs(id, addrs, pstore.PermanentAddrTTL)
+}
+
+func GenHostSwarm(t *testing.T, ctx context.Context) *bhost.BasicHost {
+	n := GenSwarmNetwork(t, ctx)
+	return bhost.New(n)
+}
+
+var RandPeerID = tu.RandPeerID
